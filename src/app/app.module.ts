46a662f5f7c38da40dc7d46a99469aab790ab579
wolfather <so.israelweb@gmail.com>
2020-09-18 21:40:21 -0300

    adicionando arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { HttpService } from 'src/service/http/http.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,12 @@ import { SecondComponent } from './second/second.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    HttpService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
