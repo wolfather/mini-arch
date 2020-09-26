44a2855fbe1c96e821fadd275df1a1b7579da137
wolfather <so.israelweb@gmail.com>
2020-09-25 21:24:45 -0300

    adicionando tabela e testes

--- src/app/custom-table/custom-table.component.spec.ts
+++ src/app/custom-table/custom-table.component.spec.ts
@@ -1,25 +1,40 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CustomTableComponent } from './custom-table.component';
 
 describe('CustomTableComponent', () => {
-  let component: CustomTableComponent;
-  let fixture: ComponentFixture<CustomTableComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CustomTableComponent ]
-    })
-    .compileComponents();
-  }));
+  const stubTable = [
+    {
+      "first-name": "John",
+      "last-name": "Doe",
+      "age": "30",
+      "email": "john@email.com",
+      "level": "1",
+    },
+  ];
+  const cp: CustomTableComponent = new CustomTableComponent();
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CustomTableComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    cp.dataTable = stubTable;
+  });
+  
+  it('should create', () => {
+    expect(cp).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should fill dataTable', () => {
+    expect(cp.dataTable.length).toBeGreaterThanOrEqual(1);
   });
+
+  it('should return the header of keys using createHeader()', () => {
+    const headerResult = cp.createHeader();
+
+    expect(headerResult).toEqual(['first-name','last-name','age','email','level']);
+  });
+
+  it('should return a new array of items on createBody()', () => {
+    const bodyResult = cp.createBody();
+
+    expect(bodyResult[0]).toEqual(["John", "Doe", "30", "john@email.com", "1"]);
+  });
+
 });
